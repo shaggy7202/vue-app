70117ca04cd38b38245115e44cdd9ddf483e325c
oleksandr_potapov <oleksandr.potapov@probegin.com>
2018-06-08 17:33:21 +0300

    Add vue basic examples

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 Vue.config.productionTip = false;
-
-
-new Vue({
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+var app = new Vue({
     delimiters:["{", "}"],
     el: '#app',
     data:{
-        msg:'Hello!'
+        msg:'Hello!',
+        groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+
+    },
+    methods:{
+        reverseMessage: function () {
+            this.msg = this.msg.split('').reverse().join('')
+        }
     },
 
 });
